f7816f96d3c1721dcb569fb7f82f17f65cda4b06
santiago ramirez henao <zanti82@gmail.com>
2022-09-01 02:31:49 +0000

    Modelos/AbstractModelo.py edited online with Bitbucket

--- Modelos/AbstractModelo.py
+++
@@ -1,8 +0,0 @@
-from abc import ABCMeta
-
-#Constructor
-
-class AbstractModelo(metaclass=ABCMeta):
-    def __init__(self, data):
-        for llave, valor in data.items():
-            setattr(self, llave, valor)
